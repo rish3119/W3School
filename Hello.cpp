85c482922256dce957ae210bc885581f9b3a3711
rish3119 <rishavsingh312001@gmail.com>
2026-06-09 23:24:09 +0530

    Hello

---
+++ Hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    getline(cin, name);
+
+    // Print the greeting lines
+
+		cout << "Hello, " << name << "!" << endl;
+    cout << "Welcome to programming!" << endl;
+  
+    return 0;
+}
